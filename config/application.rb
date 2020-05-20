d702a15b93f612ae86fcfbdb6be5c4df5dca77bd
LibreDMM <admin@libredmm.com>
2020-05-19 22:11:36 -0700

    sidekiq

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module Librefanza
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
