b3a5c4072c3906e9c74cf213cc93ec965c27532d
LibreDMM <admin@libredmm.com>
2020-05-24 22:37:04 -0700

    early abort for crawler

--- app/workers/crawl_worker.rb
+++ app/workers/crawl_worker.rb
@@ -5,10 +5,20 @@ class CrawlWorker
 
   def perform(keyword)
     logger.info "Crawling #{keyword} on Fanza"
+    failure_in_a_row = 0
     Fanza::Api.item_list(keyword, sort: "date") do |json|
-      FanzaItem.create(
+      item = FanzaItem.create(
         raw_json: json,
       )
+      if item.persisted?
+        failure_in_a_row = 0
+      else
+        failure_in_a_row += 1
+        if failure_in_a_row >= 10
+          logger.info "#{keyword}: #{failure_in_a_row} duplicates in a row, aborting crawler."
+          break
+        end
+      end
     end
   end
 end
