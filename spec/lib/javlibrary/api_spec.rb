4ca16770c614871d118798d8b1f2190f1eb6ec3c
LibreDMM <admin@libredmm.com>
2020-05-26 16:09:04 -0700

    cover them all

--- spec/lib/javlibrary/api_spec.rb
+++ spec/lib/javlibrary/api_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Javlibrary::Api" do
+RSpec.describe Javlibrary::Api do
   before(:each) do
     allow(Javlibrary::Challenger).to receive(:headers).and_return({})
   end
