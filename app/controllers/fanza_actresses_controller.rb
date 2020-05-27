ab8fa84de79afea2e4f9f58de228691368fbfce1
LibreDMM <admin@libredmm.com>
2020-05-27 14:07:16 -0700

    sorting and filtering for actress index page

--- app/controllers/fanza_actresses_controller.rb
+++ app/controllers/fanza_actresses_controller.rb
@@ -2,12 +2,29 @@ class FanzaActressesController < ApplicationController
   include ItemsAggregator
 
   def index
-    @actresses = FanzaActress.all.order(:id_fanza)
+    @actresses = FanzaActress.all
+
+    @order = params[:order]
+    case params[:order]
+    when /name/i
+      @actresses = @actresses.order(:name)
+    when /id/i
+      @actresses = @actresses.order(id_fanza: :desc)
+    else
+      @order = "ID"
+      @actresses = @actresses.order(id_fanza: :desc)
+    end
+
+    if params[:fuzzy]
+      @actresses = @actresses.where("name ILIKE ?", "%#{params[:fuzzy]}")
+    end
     @actresses = @actresses.page(params[:page])
   end
 
   def show
-    @actress = FanzaActress.find_by(id_fanza: params[:id]) || FanzaActress.new(name: params[:id])
+    @actress = FanzaActress.find_by(id_fanza: params[:id]) ||
+               FanzaActress.order(:id_fanza).find_by(name: params[:id]) ||
+               FanzaActress.new(name: params[:id])
     fanza_query = @actress.persisted? ?
       FanzaItem.where(%{raw_json @> '{"iteminfo": {"actress": [{"id": #{@actress.id_fanza}}]}}'}) :
       FanzaItem.where(%{raw_json @> '{"iteminfo": {"actress": [{"name": "#{@actress.name}"}]}}'})
