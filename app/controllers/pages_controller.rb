ae68566ecf0eb10cd1f215dd390d915cf60fa8db
LibreDMM <admin@libredmm.com>
2020-05-27 14:27:28 -0700

    fix cover image link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   def search
     keyword = Fanza::Id.normalize(params[:q])
     redirect_to Fanza::Id.normalized?(keyword) ?
-                  movie_path(keyword) :
+                  movie_path(keyword, format: params[:format]) :
                   movies_path(fuzzy: keyword)
   end
 end
