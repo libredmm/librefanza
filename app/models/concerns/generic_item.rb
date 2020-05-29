431ec811034267635a0be4d4a3cc93d9768a48eb
LibreDMM <admin@libredmm.com>
2020-05-29 14:58:15 -0700

    update movie date on item change

--- app/models/concerns/generic_item.rb
+++ app/models/concerns/generic_item.rb
@@ -3,15 +3,20 @@ module GenericItem
 
   included do
     belongs_to :movie, foreign_key: :normalized_id, primary_key: :normalized_id, optional: true
-    after_save :recreate_movie
+    after_save :create_or_update_movie
   end
 
-  def recreate_movie
-    return unless normalized_id_previously_changed?
+  def create_or_update_movie
+    if normalized_id_previously_changed?
+      old_movie = Movie.find_by(normalized_id: normalized_id_before_last_save)
+      old_movie.destroy if old_movie && !old_movie.preferred_item
+    end
 
-    old_movie = Movie.find_by(normalized_id: normalized_id_before_last_save)
-    old_movie.destroy if old_movie && !old_movie.preferred_item
-    create_movie(normalized_id: self.normalized_id)
+    if movie
+      movie.touch
+    else
+      create_movie(normalized_id: self.normalized_id)
+    end
   end
 
   def attributes
