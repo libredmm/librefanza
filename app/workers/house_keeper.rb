b95986ef16c64cc257d6f5801606382893b81390
LibreDMM <admin@libredmm.com>
2020-05-30 22:17:42 -0700

    reduce batch size for house keeping

--- app/workers/house_keeper.rb
+++ app/workers/house_keeper.rb
@@ -12,13 +12,13 @@ class HouseKeeper
     case task.to_sym
     when :fetch_html
       logger.info "Fetching missing raw HTMLs"
-      FanzaItem.where(raw_html: "").find_each(&:fetch_html!)
-      FanzaItem.where(raw_html: nil).find_each(&:fetch_html!)
+      FanzaItem.where(raw_html: "").find_each(batch_size: 100, &:fetch_html!)
+      FanzaItem.where(raw_html: nil).find_each(batch_size: 100, &:fetch_html!)
     when :derive_fields
       logger.info "Re-deriving fields"
-      FanzaItem.find_each(&:derive_fields!)
-      MgstageItem.find_each(&:derive_fields!)
-      JavlibraryItem.find_each(&:derive_fields!)
+      FanzaItem.find_each(batch_size: 100, &:derive_fields!)
+      MgstageItem.find_each(batch_size: 100, &:derive_fields!)
+      JavlibraryItem.find_each(batch_size: 100, &:derive_fields!)
     else
       logger.warn "Invalid task #{task}"
     end
