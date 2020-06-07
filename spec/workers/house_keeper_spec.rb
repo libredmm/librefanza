2c6d53f53484c1505d2763f5f79ae2f796dac34a
LibreDMM <admin@libredmm.com>
2020-06-07 15:35:19 -0700

    cleanup obsolete house keeper tasks

--- spec/workers/house_keeper_spec.rb
+++ spec/workers/house_keeper_spec.rb
@@ -1,20 +1,6 @@
 require "rails_helper"
 
 RSpec.describe HouseKeeper, type: :worker do
-  it "can fetch missing raw html" do
-    create(:fanza_item).update_column(:raw_html, "")
-    expect_any_instance_of(FanzaItem).to receive(:fetch_html!)
-    subject.perform :fetch_html
-  end
-
-  it "can re-derive actress" do
-    movie = create(:movie)
-    movie.update_column(:actress_fanza_ids, nil)
-    movie.update_column(:actress_names, nil)
-    expect_any_instance_of(Movie).to receive(:derive_fields!)
-    subject.perform :derive_actresses
-  end
-
   it "can re-derive fields" do
     create(:fanza_item)
     expect_any_instance_of(FanzaItem).to receive(:derive_fields!)
