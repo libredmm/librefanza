60b5dbd0e5c709d2c08e9723eb19995960c52cd8
LibreDMM <admin@libredmm.com>
2020-06-07 22:29:34 -0700

    destroy obsolete movie on item destroy

--- spec/models/javlibrary_item_spec.rb
+++ spec/models/javlibrary_item_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe JavlibraryItem, type: :model do
   subject { create(:javlibrary_item) }
 
   it_behaves_like "generic item"
+
+  context "on destroy" do
+    it "does not destroy movie that has other items" do
+      id = subject.normalized_id
+      create :fanza_item, content_id: id
+      subject.destroy
+      expect(Movie.where(normalized_id: id)).to exist
+    end
+  end
 end
