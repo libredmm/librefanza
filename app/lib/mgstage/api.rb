0a3e7dce10aa5b9d7654d68ebd2d17401d69f77a
LibreDMM <admin@libredmm.com>
2020-06-11 12:30:07 -0700

    do not store mgstage search page

--- app/lib/mgstage/api.rb
+++ app/lib/mgstage/api.rb
@@ -6,7 +6,6 @@ module Mgstage
       Fanza::Id.variations(keyword).each do |variation|
         search_url = "https://www.mgstage.com/search/search.php?search_word=#{variation}"
         search_page = self.get(search_url)
-        yield search_url, search_page
         Nokogiri::HTML(search_page).css("div.search_list h5 a").each do |a|
           url = URI::join(search_url, a.attr(:href)).to_s
           yield url, self.get(url)
