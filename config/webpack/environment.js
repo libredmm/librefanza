7e17d0ea1359a1e3f712f9c9b7d2554a11b8e18f
LibreDMM <admin@libredmm.com>
2021-04-03 22:12:14 -0700

    rails webpacker:install

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-const webpack = require('webpack')
-environment.plugins.append(
-    'Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default']
-    })
-)
