6b40c93b245d0968de99a543d96799bb709192fc
LibreDMM <admin@libredmm.com>
2021-04-05 14:27:43 -0700

    configure sidekiq-unique-jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,3 +2,21 @@ SidekiqUniqueJobs.configure do |config|
   config.enabled = Rails.env.production?
   config.lock_prefix = ENV["SIDEKIQ_UNIQUE_PREFIX"] || "uniquejobs"
 end
+
+Sidekiq.configure_server do |config|
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
+end
+
+Sidekiq.configure_client do |config|
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+end
