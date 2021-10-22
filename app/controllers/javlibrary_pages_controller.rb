47593548782429a511cdbe3b5330a4162c70e5e9
LibreDMM <admin@libredmm.com>
2021-10-22 16:34:11 -0700

    support for manually adding javlibrary items

--- app/controllers/javlibrary_pages_controller.rb
+++ app/controllers/javlibrary_pages_controller.rb
@@ -2,11 +2,24 @@ class JavlibraryPagesController < ApplicationController
   def index
     @pages = JavlibraryPage.order(id: :desc).all
     @pages = @pages.page(params[:page])
-    render "generic_pages/index"
   end
 
   def show
     @page = JavlibraryPage.find(params[:id])
     render html: @page.raw_html.html_safe
+  end
+
+  def new
+    @page = JavlibraryPage.new
+  end
+
+  def create
+    attrs = params.require(:javlibrary_page).permit(:url, :raw_html)
+    @page = JavlibraryPage.create(attrs)
+    if @page.persisted?
+      redirect_to (@page.javlibrary_item || @page)
+    else
+      render :new
+    end
   end
 end
--- spec/requests/javlibrary_pages_spec.rb
+++ spec/requests/javlibrary_pages_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "JavlibraryPages", type: :request do
-  let(:page) { create(:javlibrary_product_page) }
   let(:user) { create(:user) }
   let(:admin) { create(:admin) }
 
@@ -19,6 +18,8 @@ RSpec.describe "JavlibraryPages", type: :request do
   end
 
   describe "GET /javlibrary_pages/:id" do
+    let(:page) { create(:javlibrary_product_page) }
+
     it "works for admin" do
       get javlibrary_page_path(page, as: admin)
       expect(response).to have_http_status(200)
@@ -28,6 +29,61 @@ RSpec.describe "JavlibraryPages", type: :request do
       expect {
         get javlibrary_page_path(page, as: user)
       }.to raise_error(ActionController::RoutingError)
+    end
+  end
+
+  describe "GET /javlibrary_pages/new" do
+    it "works for admin" do
+      get new_javlibrary_page_path(as: admin)
+      expect(response).to have_http_status(200)
+    end
+
+    it "rejects other users" do
+      expect {
+        get new_javlibrary_page_path(as: user)
+      }.to raise_error(ActionController::RoutingError)
+    end
+  end
+
+  describe "POST /javlibrary_pages/" do
+    it "rejects non admins" do
+      expect {
+        post javlibrary_pages_path(as: user)
+      }.to raise_error(ActionController::RoutingError)
+    end
+
+    describe "with invalid params" do
+      it "renders form with errors" do
+        post javlibrary_pages_path(
+          javlibrary_page: { url: "", raw_html: "" },
+          as: admin,
+        )
+        expect(response.body).to include("invalid-feedback")
+      end
+    end
+
+    describe "when page contains item" do
+      it "redirect to item" do
+        attrs = attributes_for(:javlibrary_product_page)
+        post javlibrary_pages_path(javlibrary_page: attrs, as: admin)
+        page = JavlibraryPage.find_by(url: attrs[:url])
+        expect(response).to redirect_to(page.javlibrary_item)
+      end
+    end
+
+    describe "when page does not contain item" do
+      it "redirect to item" do
+        url = generate :url
+        post javlibrary_pages_path(
+          javlibrary_page: {
+            url: url,
+            raw_html: "<html></html>",
+          },
+          as: admin,
+        )
+        page = JavlibraryPage.find_by(url: url)
+        expect(response).to redirect_to(page)
+      end
     end
   end
 end
