1a23cacf43f59401cb9f782be3d5331104d94235
LibreDMM <admin@libredmm.com>
2023-07-10 12:28:40 -0700

    prefer new items

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe Movie, type: :model do
       another_item.update_column(:normalized_id, item.normalized_id)
 
       expect {
-        another_item.update(date: 1.day.ago)
+        another_item.update(date: 1.day.from_now)
       }.to change {
         movie.reload.date
       }
