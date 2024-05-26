ef1e3b5d31bfbff7b2aa1ac386393eb725a6dfa4
LibreDMM <admin@libredmm.com>
2024-05-26 13:37:34 -0700

    git push before deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 DEPLOY_DEST=librefanza
 
-scp .env .env.production $DEPLOY_DEST:~/librefanza/
+git push &&
+scp .env .env.production $DEPLOY_DEST:~/librefanza/ &&
 ssh $DEPLOY_DEST "cd ~/librefanza && git pull --rebase && sudo docker compose up --build -d"
