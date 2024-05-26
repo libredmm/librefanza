8608acf1e69105cdeb44d1a9908bfb54a79c49a0
LibreDMM <admin@libredmm.com>
2024-05-26 13:36:58 -0700

    fix sidekiq env

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 DEPLOY_DEST=librefanza
 
 scp .env .env.production $DEPLOY_DEST:~/librefanza/
-ssh $DEPLOY_DEST "cd ~/librefanza && git pull --rebase"
+ssh $DEPLOY_DEST "cd ~/librefanza && git pull --rebase && sudo docker compose up --build -d"
