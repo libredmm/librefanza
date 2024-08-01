f100527f87250ab5621743bc94785bbacb94ba83
LibreDMM <admin@libredmm.com>
2024-08-01 13:43:23 -0700

    remove deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DEPLOY_HOST="dockyard"
-DEPLOY_PATH="~/librefanza"
-
-git push &&
-scp .env .env.production $DEPLOY_HOST:$DEPLOY_PATH &&
-ssh $DEPLOY_HOST "cd $DEPLOY_PATH && git pull --rebase && sudo docker compose down && sudo docker compose up --build -d"
